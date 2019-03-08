871184f24d7a8ff7a860f5807a47f647b77c270b
Martin Sehnoutka <msehnout@redhat.com>
2019-03-08 14:38:06 +0100

    modify SQL query to follow documentation of the library

--- f8a_report/epv_report.py
+++ f8a_report/epv_report.py
@@ -19,18 +19,31 @@ class EPVReport:
 
     def __init__(self):
         """Create an empty report."""
-        self.rds = Postgres()  # TODO: error handling? There is no in the class
+        self.pg = Postgres()  # TODO: error handling? There is no in the class
+
+    @staticmethod
+    def epv_tuple_into_dict(epv):
+        return {
+            'ecosystem': epv[0],
+            'package': epv[1],
+            'version': epv[2]
+        }
 
     def get_list_of_scheduled_ingestions(self):
         """Connect to RDS and get a list of scheduled ingestions."""
-        QUERY = r"SELECT EC.NAME, PK.NAME, VR.IDENTIFIER " \
-                r"FROM ANALYSES AN, PACKAGES PK, ECOSYSTEMS EC " \
-                r"WHERE AN.STARTED_AT >= '{}' AND " \
-                r"WHERE AN.STARTED_AT < '{}' AND " \
-                r"AN.VERSION_ID = VR.ID AND VR.PACKAGE_ID = PK.ID AND PK.ECOSYSTEM_ID = EC.ID"
-        query = sql.SQL(QUERY).format(
-            sql.Literal("2018-12-21"), sql.Literal("2018-12-25")
-        )
+        # source: http://initd.org/psycopg/docs/usage.html#passing-parameters-to-sql-queries
+        self.pg.cursor.execute("""
+                               SELECT EC.NAME, PK.NAME, VR.IDENTIFIER
+                               FROM ANALYSES AN, PACKAGES PK, ECOSYSTEMS EC
+                               WHERE AN.STARTED_AT >= %s
+                               AND WHERE AN.STARTED_AT < %s
+                               AND AN.VERSION_ID = VR.ID
+                               AND VR.PACKAGE_ID = PK.ID
+                               AND PK.ECOSYSTEM_ID = EC.ID
+                               """,
+                               ("2018-12-21", "2018-12-25"))
+        raw_data = self.pg.cursor.fetchall()
+        return list(map(EPVReport.epv_tuple_into_dict, raw_data))
 
     def get_list_of_failed_ingestions(self):
         """Connect to RDS and get a list of failed ingestions."""
