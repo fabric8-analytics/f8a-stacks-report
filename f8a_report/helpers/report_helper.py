aff8313eefb25ff6abe5b0d4261447aded176edd
rafiu007 <rafiu.jaman1997@gmail.com>
2020-12-15 15:13:10 +0530

    Updated cleanup function to take column_name as argument.

--- f8a_report/helpers/report_helper.py
+++ f8a_report/helpers/report_helper.py
@@ -64,13 +64,15 @@ class ReportHelper:
 
         self.emr_api = os.getenv('EMR_API', 'http://f8a-emr-deployment:6006')
 
-    def cleanup_tables(self, table_name, num_days):
+    def cleanup_tables(self, table_name, column_name, num_days):
         """Cleanup tables on a periodic basis."""
-        # query to delete data
-        query = sql.SQL("""DELETE FROM {} WHERE DATE_DONE <= NOW() - interval '%s day';""").format
-        (sql.Identifier(table_name))
+        # Query to delete data
+        query = sql.SQL('DELETE FROM {0} WHERE {1} <= NOW() - interval \'%s day\';').format(
+            sql.Identifier(table_name),
+            sql.Identifier(column_name)
+        )
         logger.debug('Starting to clean up "%s" table', table_name)
-        self.cursor.execute(query, num_days)
+        self.cursor.execute(query.as_string(self.conn) % (num_days))
         self.conn.commit()
         # Log the message returned from db cursor
         logger.info('Cleanup of  "%s" table has completed with status %r', table_name,
@@ -80,22 +82,22 @@ class ReportHelper:
         """Cleanup RDS data tables on a periodic basis."""
         try:
             num_days = os.environ.get('KEEP_DB_META_NUM_DAYS', '30')
-            self.cleanup_tables('celery_taskmeta', num_days)
+            self.cleanup_tables('celery_taskmeta', 'date_done', num_days)
 
             num_days = os.environ.get('KEEP_WORKER_RESULT_NUM_DAYS', '30')
-            self.cleanup_tables('worker_results', num_days)
+            self.cleanup_tables('worker_results', 'ended_at', num_days)
 
             num_days = os.environ.get('KEEP_PACKAGE_ANALYSES_NUM_DAYS', '30')
-            self.cleanup_tables('package_analyses', num_days)
+            #self.cleanup_tables('package_analyses', 'finished_at', num_days)
 
             num_days = os.environ.get('KEEP_PACKAGE_WORKER_RESULT_NUM_DAYS', '30')
-            self.cleanup_tables('package_worker_results', num_days)
+            #self.cleanup_tables('package_worker_results','ended_at', num_days)
 
             num_days = os.environ.get('KEEP_STACK_ANALYSES_REQUESTS_NUM_DAYS', '180')
-            self.cleanup_tables('stack_analyses_request', num_days)
+            self.cleanup_tables('stack_analyses_request','submitTime', num_days)
 
             num_days = os.environ.get('KEEP_API_REQUESTS_NUM_DAYS', '180')
-            self.cleanup_tables('api_requests', num_days)
+            self.cleanup_tables('api_requests','submit_time', num_days)
         except Exception as e:
             logger.error('CleanupDatabaseError: %r' % e)
 
