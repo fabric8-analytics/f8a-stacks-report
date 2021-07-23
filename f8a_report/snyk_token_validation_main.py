5b031ad8d969f5edeae7026807733484983082d2
jparsai <jparsai@redhat.com>
2021-07-23 16:01:56 +0530

    Trigger user caching. (#214)

--- f8a_report/snyk_token_validation_main.py
+++ f8a_report/snyk_token_validation_main.py
@@ -3,8 +3,15 @@
 from f8a_report.helpers.db_gateway import TokenValidationQueries
 from f8a_utils.user_token_utils import decrypt_api_token, is_snyk_token_valid
 import logging
+import requests
+import tenacity
+import os
 
 logger = logging.getLogger(__file__)
+
+_USER_CACHE_API_URL = "http://{host}:{port}/internal/ingestions/refresh_user_cache".format(
+    host=os.environ.get("CACHE_SERVICE_HOST", "bayesian-jobs"),
+    port=os.environ.get("CACHE_SERVICE_PORT", "34000"),)
 
 
 def main():
@@ -12,6 +19,7 @@ def main():
     user_to_tokens = TokenValidationQueries().get_registered_user_tokens()
     unregistered_users = call_snyk_api(user_to_tokens)
     TokenValidationQueries().update_users_to_unregistered(unregistered_users)
+    cache_all_users()
 
 
 def call_snyk_api(user_to_tokens: dict) -> list:
@@ -24,6 +32,17 @@ def call_snyk_api(user_to_tokens: dict) -> list:
             unregistered_users.append(user_id)
 
     return unregistered_users
+
+
+@tenacity.retry(reraise=True, stop=tenacity.stop_after_attempt(3), wait=tenacity.wait_fixed(1))
+def cache_all_users():
+    """Cache all users into PVC."""
+    logger.info("Invoking API to create user cache.")
+    try:
+        response = requests.get(_USER_CACHE_API_URL)
+        logger.info(response.json())
+    except Exception as e:
+        logger.error(e)
 
 
 if __name__ == '__main__':
